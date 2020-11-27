2777c361839784ecded50f85b4db31481f5adfec
jay8anks <75101188+jay8anks@users.noreply.github.com>
2020-11-26 18:37:22 -0600

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,193 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/design2.css?rnd=139" rel="stylesheet" />
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <style>
+    
+        .auto-style1 {
+            width: 179px;
+            text-align: right;
+            font-weight: bold;
+        }
+    
+
+        .auto-style4 {
+            height: 38px;
+        }
+        .auto-style6 {
+            width: 56%;
+            background-color: white;
+        }
+        .cntrl-div {
+            width: 60%;
+            background-color: white;  
+            border: solid 1px black;
+        }
+
+        .auto-style7 {
+            width: 179px;
+            text-align: right;
+            font-weight: bold;
+            height: 26px;
+        }
+        .auto-style8 {
+            height: 26px;
+        }
+
+
+        .collapsible {
+             background-color: black;
+             color: white;
+             cursor: pointer;
+             padding: 5px;
+             width: 100%;
+             border: none;
+             text-align: left;
+             outline: none;
+             font-size: 15px;
+         }
+
+         .active, .collapsible:hover {
+                 background-color: black;
+         }
+
+         .content {
+             padding: 0 18px;
+             display: none;
+             overflow: hidden;
+             background-color: #f1f1f1;
+         }
+
+       
+    </style>
+
+      <script type="text/javascript">
+         function collapseExpand() {
+             document.getElementById("collapsible-button0").click();
+         }        
+     </script>
+
+</head>
+<body onload="collapseExpand();">
+    <form id="form1" runat="server">
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+<a class="navbar-brand" href="#">MyTecBits.com</a>
+<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div id="navbar" class="navbar-collapse collapse">
+<ul class="navbar-nav mr-auto">
+<li class="nav-item active"><a class="nav-link" href="/Default.aspx">Home</a></li>
+<li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+<li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="dropdown04">Dropdown <span class="caret"></span></a>
+<div class="dropdown-menu" aria-labelledby="dropdown04">
+<a class="dropdown-item" href="#">Action</a>
+<a class="dropdown-item" href="#">Another action</a>
+<a class="dropdown-item" href="#">Something else here</a>
+</div>
+</li>
+</ul>
+</div>
+<!–/.nav-collapse –>
+</nav>
+        <div class="container">
+            <br />
+            <br />
+            <br />
+            <br />
+            <button type="button" id="collapsible-button0" class="collapsible">Add&nbsp;User </button>
+            <div class="content">
+               <br />
+             <div class="cntrl-div"><br />
+              <table class="tbl-class">
+                <tr>
+                    <td class="auto-style1">UserName:&nbsp; </td>
+                    <td class="auto-style6"><asp:TextBox ID="TextBox1" class="form-control" autocomplete="off" runat="server" Height="30px" Width="200px" MaxLength="40"></asp:TextBox>
+
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBox1" Display="Dynamic" ErrorMessage="*Required" ForeColor="Red" ValidationGroup="reqField"></asp:RequiredFieldValidator>
+
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style1">First Name:&nbsp; </td>
+                    <td class="auto-style6"><asp:TextBox ID="TextBox2" class="form-control" autocomplete="off" runat="server" Height="30px" Width="200px"></asp:TextBox>
+
+                    </td>
+                    <td class="auto-style4"></td>
+                </tr>
+                <tr>
+                    <td class="auto-style1">Last Name:&nbsp; </td>
+                    <td class="auto-style6"><asp:TextBox ID="TextBox3" class="form-control" autocomplete="off" runat="server" Height="30px" Width="200px"></asp:TextBox>
+
+                    </td>
+                    <td class="auto-style4"></td>
+                </tr>
+                <tr>
+                    <td class="auto-style1">&nbsp;</td>
+                    <td class="bg-white" colspan="2"><asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add User" CommandName="Insert" Width="200px" ValidationGroup="reqField" />&nbsp;&nbsp;<asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Add Another" CommandName="Insert" Visible="False" Width="188px" /></td>
+                </tr>
+                <tr>
+                    <td class="auto-style7"></td>
+                    <td colspan="2" class="auto-style8">
+            <asp:Label ID="Label1" runat="server" ForeColor="Red"></asp:Label>
+
+                    </td>
+                </tr>
+            </table>
+             </div> 
+            <br />
+                </div>
+            
+            
+             <br />
+        <br /><%-- source: https://parallelcodes.com/styling-the-gridview-in-asp-net-2/--%>
+        <asp:GridView ID="GridView1" runat="server" CssClass="mydatagrid" PagerStyle-CssClass="pager"
+            HeaderStyle-CssClass="header" RowStyle-CssClass="rows" AllowPaging="True" OnPageIndexChanging="Gridview1_PageIndexChanging" OnSelectedIndexChanged="Gridview1_SelectedIndexChanged">
+        </asp:GridView>
+            <asp:LinkButton ID="LinkButton1" runat="server" OnClick="LinkButton1_Click">Delete All</asp:LinkButton>
+            <br />
+            <br />
+&nbsp;<br />
+        </div>
+
+        
+     
+
+
+   <script>
+       var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
+   </script> 
+
+        <%--Bootstrap demo from https://www.mytecbits.com/microsoft/dot-net/how-to-add-bootstrap-in-asp-net#WebFormsManual--%>
+
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SQLite;
+using System.Data;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+            GetUniqueId();
+            BindGridView();            
+        }
+    }
+
+    protected void GetUniqueId()
+    {
+        SQLiteConnection Con = new SQLiteConnection(ConfigurationManager.ConnectionStrings["dbSqlite"].ConnectionString);
+        bool uidExist = false;
+        string uniqueId = UniqueId();
+
+        using (Con)
+        {
+            Con.Open();
+            try
+            {
+                uidExist = ValueExist(Con, uniqueId);
+            }
+            catch
+            {
+                // log error.
+            }
+
+            if (uidExist == false)
+            {
+                ViewState["UniqueId"] = uniqueId;
+                return;
+            }
+            else
+            {
+                int i = 0;
+                do
+                {
+                    uniqueId =  UniqueId();
+                    uidExist = ValueExist(Con, uniqueId);
+                    if (uidExist == false)
+                    {
+                        ViewState["UniqueId"] = uniqueId;
+                        return;
+                    }
+
+                    i++;
+
+                } while (i < 5);
+
+                // If you can't get a unique uid in 5 tries
+                // give the user a cryptic error message
+                Label1.Text = "System Error";
+            }
+        }
+    }
+
+    protected void BindGridView()
+    {
+        DataTable dt = new DataTable();
+
+        SQLiteConnection Con = new SQLiteConnection(ConfigurationManager.ConnectionStrings["dbSqlite"].ConnectionString);
+
+        string sql = "Select UserName, FirstName, LastName, strftime('%m-%d-%Y', Created) as Created, Created_by From User";
+        SQLiteCommand cmd = new SQLiteCommand(sql, Con);
+        SQLiteDataAdapter MySqlDa = new SQLiteDataAdapter(cmd);
+
+        using (Con)
+        {
+            Con.Open();
+            try
+            {
+
+                MySqlDa.Fill(dt);
+
+
+            }
+             catch (Exception ex)
+            {
+              // Log Error
+
+            }
+        }
+
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+
+        if (dt.Rows.Count > 0)
+        {
+            LinkButton1.Visible = true;
+        }
+        else
+        {
+            LinkButton1.Visible = false;
+        }
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Button btnSelect = (sender as Button);
+        string commandName = btnSelect.CommandName;
+        UserAddUpdate(commandName);
+
+        BindGridView();
+        Button3.Visible = true;
+
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        ResetControls();
+    }
+
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        DeleteUsers();
+        BindGridView();
+    }
+
+    #region Data Access
+
+    protected void UserAddUpdate(string commandName)
+    {
+        SQLiteConnection Con = new SQLiteConnection(ConfigurationManager.ConnectionStrings["dbSqlite"].ConnectionString);
+        string userName = TextBox1.Text.Trim().ToLower();
+        string uniqueId = (string)ViewState["UniqueId"];
+
+        if (commandName == "Insert")
+        {
+            using (Con)
+            {
+                Con.Open();
+
+                // Check if userName exist, exit if it does.
+                bool userExist = UserExist(Con, userName);
+                if (userExist == true)
+                {
+                    Label1.Text = "Duplicate User Name.";
+                    Button3.Visible = false;
+                    TextBox1.Enabled = false;
+                    return;
+                }
+                else
+                {
+                    Label1.Text = string.Empty;
+                }
+
+                bool valueExist = false;
+                valueExist = ValueExist(Con, uniqueId);
+
+                if (valueExist == false)
+                {
+                    InsertNewUser(Con, uniqueId);
+                    TextBox1.Enabled = false;
+                }
+                else
+                {
+                    Label1.Text = "Insert Error";
+                    return;
+                }
+               
+                UpdateNewUser(Con, uniqueId);
+            }
+
+            Button1.CommandName = "Update";
+            Button1.Text = "Update User";
+
+        }
+        else if (commandName == "Update")
+        {
+            using (Con)
+            {
+                Con.Open();
+                UpdateNewUser(Con, uniqueId);
+            }
+        }
+    }
+
+    protected void InsertNewUser(SQLiteConnection Con, string uniqueId)
+    {
+        string created = DateTime.Now.ToString("yyyy-MM-dd");
+        string created_by = "User.Identity"; // User.Identity.Name, After you set up some type of authentication.
+
+
+        string sql = @"Insert Into `User` (Uid, UserName, Created, Created_by ) values(@Uid, @UserName, @Created, @Created_by)";
+        string userName = TextBox1.Text.Trim().ToLower();
+
+        using (SQLiteCommand cmd = new SQLiteCommand(sql, Con))
+        {
+
+           try
+            {
+                cmd.Parameters.AddWithValue("@uid", uniqueId);
+                cmd.Parameters.AddWithValue("@UserName", userName);
+                cmd.Parameters.AddWithValue("@Created", created);
+                cmd.Parameters.AddWithValue("@Created_by", created_by);
+                cmd.ExecuteNonQuery();
+            }
+           catch
+            {
+            // log error;
+            }
+        }
+    }
+
+    protected void UpdateNewUser(SQLiteConnection Con, string uniqueId)
+    {
+        string userName = TextBox1.Text.Trim().ToLower();
+        string firstName = TextBox2.Text.Trim();
+        string lastName = TextBox3.Text.Trim();
+       
+
+
+        string sql = @"Update `User` set UserName = @UserName, FirstName = @FirstName, LastName = @LastName Where uid = @uid ";
+
+        using (SQLiteCommand cmd = new SQLiteCommand(sql, Con))
+        {
+
+            try
+            {
+                cmd.Parameters.AddWithValue("@UserName", userName);
+                cmd.Parameters.AddWithValue("@FirstName", firstName);
+                cmd.Parameters.AddWithValue("@LastName", lastName);
+                cmd.Parameters.AddWithValue("@uid", uniqueId);
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                // log error;
+            }
+        }
+
+    }
+
+  
+    protected void DeleteUsers()
+    {
+        SQLiteConnection Con = new SQLiteConnection(ConfigurationManager.ConnectionStrings["dbSqlite"].ConnectionString);
+
+        string sql = @"Delete From `User`";
+
+        using (SQLiteCommand cmd = new SQLiteCommand(sql, Con))
+        {
+            Con.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                // log error;
+            }
+        }
+    }
+
+
+    protected bool ValueExist(SQLiteConnection Con, string uniqueId)
+    {
+        object valueExist;
+        string sql = @"SELECT `uid` FROM `User` where uid = @uid";
+
+        using (SQLiteCommand cmd = new SQLiteCommand(sql, Con))
+        {
+
+            try
+            {
+                cmd.Parameters.AddWithValue("@uid", uniqueId);
+                valueExist = cmd.ExecuteScalar();
+                {
+                    if (valueExist != null)
+                        return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    protected bool UserExist(SQLiteConnection Con, string userName)
+    {
+        object userExist;
+        string sql = @"SELECT `UserName` FROM `User` where UserName = @UserName";
+
+        using (SQLiteCommand cmd = new SQLiteCommand(sql, Con))
+        {
+
+            try
+            {
+                cmd.Parameters.AddWithValue("@UserName", userName);
+                userExist = cmd.ExecuteScalar();
+                {
+                    if (userExist != null)
+                        return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    protected string UniqueId()
+    {
+        Random r = new Random();
+        string UniqueId = r.Next(10000000, 99999999).ToString();
+        return UniqueId;
+    }
+
+    protected void ResetControls()
+    {
+        GetUniqueId();
+        Button1.CommandName = "Insert";
+        Button1.Text = "Add User";
+        TextBox1.Text = string.Empty;
+        TextBox1.Enabled = true;
+        TextBox2.Text = string.Empty;
+        TextBox3.Text = string.Empty;
+        Button3.Visible = false;
+        Label1.Text = string.Empty;
+
+    }
+
+    #endregion
+
+    #region Gridview events
+
+    protected void Gridview1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void Gridview1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        try
+        {
+            BindGridView();
+            GridView1.PageIndex = e.NewPageIndex;
+            GridView1.DataBind();
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    #endregion
+
+
+   
+}
+
+
+   
